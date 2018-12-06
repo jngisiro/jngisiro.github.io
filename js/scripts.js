16c0f4088facad7b1ee19fdd80dacb4358bb53d5
Paul Ngisiro <jngisiro@gmail.com>
2018-12-06 12:13:04 +0300

    fixing ui

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,3 @@
-var radios = document.querySelectorAll('input[type=radio][name="record-type"]');
-
-function changeHandler(event) {
-   if ( this.value === 'red_flag' ) {
-     console.log('value', 'red_flag');
-   } else if ( this.value === 'inteverntion' ) {
-      console.log('value', 'inteverntion');
-   }
+function navigateToHome(){
+   
 }
-
-Array.prototype.forEach.call(radios, function(radio) {alert("running")
-   radio.addEventListener('change', changeHandler);
-});
