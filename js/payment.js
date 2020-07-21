b043e62c9fc981597b711a9cabb3bffe43d4fd1d
Paul Ngisiro <jngisiro@gmail.com>
2020-07-21 17:57:04 +0300

    Fix: remove content-type header

--- js/payment.js
+++ js/payment.js
@@ -78,10 +78,7 @@ const makePayment = async () => {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        headers: {
-          'Content-Type': 'application/json',
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
+
         redirect: 'follow', // manual, *follow, error
         referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(payload), // body data type must match "Content-Type" header
